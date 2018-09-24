7db8d4db109113b1f2269be71090695dc7615263
Curtis Shewchuk <curtis.shewchuk@queensu.ca>
2018-09-24 16:05:55 -0400

    staging all changes

---
+++ cmake-build-debug/CMakeFiles/454-design-project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/454-design-project.dir/main.cpp.o"
+  "454-design-project.pdb"
+  "454-design-project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/454-design-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -2,3 +2,13 @@
 // Created by Curtis Shewchuk on 2018-09-24.
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+
+    return 0;
+}
+
+
