34d69cb64c2a7daed3b4c022906cb20d39f41a3e
G.Staples <griffinstaples@hotmail.com>
2018-10-09 09:53:46 -0400

    AirHockeySim

---
+++ C++TestProject/Striker.hpp
@@ -0,0 +1,14 @@
+//
+//  Striker.hpp
+//  C++TestProject
+//
+//  Created by Griffin Staples on 2018-10-05.
+//  Copyright © 2018 Clarity. All rights reserved.
+//
+
+#ifndef Striker_hpp
+#define Striker_hpp
+
+#include <stdio.h>
+
+#endif /* Striker_hpp */
