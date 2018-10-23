89a19a781a677741b075457059774aaf50ca8e56
G.Staples <griffinstaples@hotmail.com>
2018-10-23 10:00:37 -0400

    Template

---
+++ Pong/main copy.cpp
@@ -0,0 +1,97 @@
+//
+//  main.cpp
+//  ENPH454Template
+//
+//  Created by Griffin Staples on 2018-10-21.
+//  Copyright © 2018 Clarity. All rights reserved.
+//
+
+#include <iostream>
+#include <math.h>
+
+////////////INITIALIZATION///////////
+//import necessary pixy+steppermotor+ai libraries
+
+/*
+ 
+ DECLARE PINS
+ 
+ //Puck Variables
+ int xPuck; float vxPuck;
+ int yPuck; float vyPuck;
+ float rPuck;
+ 
+ //Opponent Striker Variables
+ int xStriker; float vxStriker;
+ int yStriker; float vyStriker;
+ float rPaddle;
+
+ //AI Variables
+ int xAI; float vxAI;
+ int yAI; float vyAI;
+
+ //Rink Boundaries
+ int Left; int Right; int Top; int Bottom;
+ int topNetY; int bottomNetY;
+ 
+ //Stepper Motors
+ 
+ const int stepPerRev = 360;
+ 
+ 
+ 
+ 
+ //DEFINE USEFUL VARIABLES THAT NEED TO BE CALCULATED
+ int rinkMiddleX;
+ int yInt;
+ int tInt;
+ 
+ ///////////VOID SETUP/////////////
+ 
+ DECLARE PINMODES
+ INITIALIZE STEPPERS
+ INITIALIZE PIXY AND SET SETTINGS
+ 
+ 
+ 
+ 
+ 
+ */
+
+
+
+
+
+
+
+
+
+//////////VOID LOOP////////////
+int main(){
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+void setSpeed(float vx, float vy){
+    //set stepper motors such that speed of AI comes to (vx,vy);
+}
+
+void predictY(int xPuck, int yPuck, float vxPuck, float vyPuck, int xAI,
+              int xStriker, int yStriker, float vxStriker, float vyStriker,
+              int yInt, int tInt){
+    
+}
+
+void chooseMode(int xPuck, int yPuck, float vxPuck, float vyPuck, int xAI,
+                int yInt, int tInt){
+    
+}
